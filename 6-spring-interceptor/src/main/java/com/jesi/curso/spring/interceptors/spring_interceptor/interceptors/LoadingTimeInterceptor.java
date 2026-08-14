b9e856af436b2268f00a2308b0b24ade4cbe30f5
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2026-08-14 04:51:31 -0300

    proyecto de practica de interceptores http

---
+++ 6-spring-interceptor/src/main/java/com/jesi/curso/spring/interceptors/spring_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -0,0 +1,9 @@
+package com.jesi.curso.spring.interceptors.spring_interceptor.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoadingTimeInterceptor implements HandlerInterceptor {
+//la interfaz HandlerInterceptor tiene 3 metodos que se pueden implementar, preHandle, postHandle y afterCompletion
+//esos metodos no son obligatorios, se pueden implementar solo los que se necesiten ademas 
+// ya tienen implementaciones por defecto, por lo que no es necesario implementar todos los metodos
+}
---
+++ 6-spring-interceptor/src/test/java/com/jesi/curso/spring/interceptors/spring_interceptor/SpringInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesi.curso.spring.interceptors.spring_interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
